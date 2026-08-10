0de14f8a6f98ac22427973ffefbfa390655c0e68
DiyaManocha <145923872+DiyaManocha@users.noreply.github.com>
2026-08-10 13:21:52 +0530

    g-tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -284,6 +285,19 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YKR2VSBGCW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YKR2VSBGCW');
+          `}
+        </Script>
       </body>
     </html>
   );
